bf320d27bd72bf3db18e47af0657fdcc03b5ce37
Shashank S V N <shashank.svn@robosoftin.com>
2021-07-23 15:42:31 +0530

    readded deployables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://844a2b84-9f61-485d-871a-e9ce7176dc60.mock.pstmn.io/v1'
+  baseUrl: 'https://a8c334c8-3ed6-48d6-a6a3-ab91eeb6b71f.mock.pstmn.io/v1'
 };
